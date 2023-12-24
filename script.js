3817dec2a95aec65ff716aa0348a98189d2ba1a1
chrispynuggetz <133781296+chrispynuggetz@users.noreply.github.com>
2023-12-23 19:00:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Get a reference to the button element by its ID
+var myButton = document.getElementById("myButton");
 
-        console.log('hi');
-    function handleClick(){
-      console.log('test');
-    }
+// Add a click event listener to the button
+myButton.addEventListener("click", function() {
+    // Code to execute when the button is clicked
+    alert("Button clicked!");
+});
